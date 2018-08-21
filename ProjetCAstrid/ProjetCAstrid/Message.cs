7b025c938798ac4cd9296bc3856b2f03332637ca
EDG\astrid.bossy <bossy.astrid@gmail.com>
2018-08-21 09:19:00 +0200

    properties Message

--- ProjetCAstrid/ProjetCAstrid/Message.cs
+++ ProjetCAstrid/ProjetCAstrid/Message.cs
@@ -12,7 +12,27 @@ namespace ProjetCAstrid
         {
             get 
             {
-                String memo = "La vie est belle";
+                String memo = string.Empty;
+                DateTime localDate = DateTime.Now;
+                
+                if (localDate.DayOfWeek == DayOfWeek.Sunday || localDate.DayOfWeek == DayOfWeek.Saturday && localDate.Hour < 9 && localDate.Hour > 18)
+                {
+                    memo = String.Format("Bon Week-end : {0}", Environment.UserName);
+                }
+                else if (localDate.Hour < 13)
+                {
+                    memo = String.Format("Bonjour : {0}", Environment.UserName);
+                }
+                else if (localDate.Hour > 13 && localDate.Hour < 18)
+                {
+                    memo = String.Format("Bonne Après - Midi : {0}", Environment.UserName);
+                }
+                else
+                {
+                    memo = String.Format("Bonsoir !");
+                }
+
+                memo += String.Format(" On est: {0} {1}", localDate.DayOfWeek, localDate.TimeOfDay);
                 return memo;
             }
              
